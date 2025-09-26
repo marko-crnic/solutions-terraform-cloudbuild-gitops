e9d2d92f4136e9efe9e00fdd7db0d955d46e8029
marko-crnic <marko.crnic@devoteam.com>
2025-09-26 13:29:18 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="marko-demo-prj"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "marko-demo-prj-tfstate"
     prefix = "env/prod"
   }
 }
